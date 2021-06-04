5800e2e11894816c5cccb5e8b242a4d8a0c4451c
yao <ywang305@syr.edu>
2021-06-04 00:22:07 -0400

    tune up acct service

--- src/controller/accountsController.js
+++ src/controller/accountsController.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const {
-    createAccts,
-    decrypt,
+    createAcct,
+    encryptToAcctKeyStore,
+    decryptFromAcctKeyStore,
     getEthBalance,
 } = require('../service/accountsService');
 
@@ -12,14 +13,31 @@ const {
 router.post('/accounts', async (req, res, next) => {
     const { number = 0, password } = req.body;
 
-    const accts = createAccts(Number(number), password);
+    const accts = Array.from({ length: number }, () => {
+        const acct = createAcct();
+        return acct;
+    });
     res.json(accts);
+});
+
+router.post('/keystores', async (req, res) => {
+    const { number = 0, password } = req.body;
+    const keyStores = Array.from({ length: number }, () => {
+        const acct = createAcct();
+        //加密私钥
+        const keyStore = encryptToAcctKeyStore(acct, password);
+        return keyStore;
+    });
+    res.json(keyStores);
 });
 
 router.post('/accounts/decrypt/:password', async (req, res) => {
     const keystoreJson = req.body;
     const { password } = req.params;
-    const { address, privateKey } = decrypt(keystoreJson, password);
+    const { address, privateKey } = decryptFromAcctKeyStore(
+        keystoreJson,
+        password
+    );
     res.json({ address, privateKey });
 });
 
--- src/service/accountsService.js
+++ src/service/accountsService.js
@@ -5,10 +5,19 @@ function createAccts(numberOfAccts = 0, password = null) {
         let acct = accounts.create();
         //加密私钥
         if (password) {
-            acct = encrypt(acct.privateKey, password);
+            acct = encrypt(ç.privateKey, password);
         }
         return acct;
     });
+}
+
+function createAcct() {
+    return accounts.create();
+}
+
+function encryptToAcctKeyStore(acct, password = '') {
+    const keystoreJson = accounts.encrypt(acct.privateKey, password);
+    return keystoreJson;
 }
 
 function encrypt(privateKey, password) {
@@ -16,7 +25,7 @@ function encrypt(privateKey, password) {
     return keystoreJson;
 }
 
-function decrypt(keystoreJson, password) {
+function decryptFromAcctKeyStore(keystoreJson, password) {
     const { address, privateKey } = accounts.decrypt(keystoreJson, password);
     return { address, privateKey };
 }
@@ -27,4 +36,9 @@ async function getEthBalance(address) {
     return utils.fromWei(balance, 'ether');
 }
 
-module.exports = { createAccts, decrypt, getEthBalance };
+module.exports = {
+    createAcct,
+    encryptToAcctKeyStore,
+    decryptFromAcctKeyStore,
+    getEthBalance,
+};
